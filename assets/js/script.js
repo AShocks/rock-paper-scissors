059a5369f5f1d875cb438edd2d10810e4a9eada5
AShocks <oshaughnessy.adrian@gmail.com>
2022-05-21 02:04:19 +0000

    Add Docstrings to playGame and increment score functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 /**
  * Add event listeners to all the buttons
  */
- document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByClassName("btn");
     for (let button of buttons) {
         button.addEventListener("click", function () {
             let playerChoice = this.getAttribute("data-type");
             playGame(playerChoice);
 
-            
-                
-            
+
+
+
         });
     }
 });
@@ -19,43 +19,43 @@
 
 function playGame(playerChoice) {
     let choices = ["Rock", "Paper", "Scissors"];
-    let randomComputerChoice = choices[Math.floor(Math.random() * choices.length)];
-    if (randomComputerChoice === "Rock" && playerChoice === "Rock") {
-        alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerChoice} \nIt"s a draw!`);
-            
-        } else if (randomComputerChoice === "Rock" && playerChoice === "Paper") {
-            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerChoice} \nYou won!`);
-            incrementPlayerScore();
-        
-        } else if (randomComputerChoice === "Rock" && playerChoice === "Scissors") {
-            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerChoice} \nYou lost!\n`);
-            incrementComputerScore();
-        
-        } else if (randomComputerChoice === "Paper" && playerChoice === "Rock") {
-            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerChoice} \nYou lost!\n`);
-            incrementComputerScore();
-        
-        } else if (randomComputerChoice === "Paper" && playerChoice === "Paper") {
-            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerChoice} \nIt"s a draw!`);
-            
-        } else if (randomComputerChoice === "Paper" && playerChoice === "Scissors") {
-            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerChoice} \nYou won!`);
-            incrementPlayerScore();
-        
-        } else if (randomComputerChoice === "Scissors" && playerChoice === "Rock") {
-            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerChoice} \nYou won!`);
-            incrementPlayerScore();
-        
-        } else if (randomComputerChoice === "Scissors" && playerChoice === "Paper") {
-            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerChoice} \nYou lost!\n`);
-            incrementComputerScore();
-        
-        } else if (randomComputerChoice === "Scissors" && playerChoice === "Scissors") {
-            alert(`You played: ${playerChoice} \nThe computer played: ${randomComputerChoice} \nIt"s a draw!`);
-            
-        } else {
-            alert(`Game error, please try again later.`);
-            throw `Game error! Please investigate`;
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    if (computerChoice === "Rock" && playerChoice === "Rock") {
+        alert(`You chose: ${playerChoice} \nThe computer chose: ${computerChoice} \nIt"s a draw!`);
+
+    } else if (computerChoice === "Rock" && playerChoice === "Paper") {
+        alert(`You chose: ${playerChoice} \nThe computer chose: ${computerChoice} \nYou won!`);
+        incrementPlayerScore();
+
+    } else if (computerChoice === "Rock" && playerChoice === "Scissors") {
+        alert(`You chose: ${playerChoice} \nThe computer chose: ${computerChoice} \nComputer won!\n`);
+        incrementComputerScore();
+
+    } else if (computerChoice === "Paper" && playerChoice === "Rock") {
+        alert(`You chose: ${playerChoice} \nThe computer chose: ${computerChoice} \nComputer won!\n`);
+        incrementComputerScore();
+
+    } else if (computerChoice === "Paper" && playerChoice === "Paper") {
+        alert(`You chose: ${playerChoice} \nThe computer chose: ${computerChoice} \nIt"s a draw!`);
+
+    } else if (computerChoice === "Paper" && playerChoice === "Scissors") {
+        alert(`You chose: ${playerChoice} \nThe computer chose: ${computerChoice} \nYou won!`);
+        incrementPlayerScore();
+
+    } else if (computerChoice === "Scissors" && playerChoice === "Rock") {
+        alert(`You chose: ${playerChoice} \nThe computer chose: ${computerChoice} \nYou won!`);
+        incrementPlayerScore();
+
+    } else if (computerChoice === "Scissors" && playerChoice === "Paper") {
+        alert(`You chose: ${playerChoice} \nThe computer chose: ${computerChoice} \nComputer won!\n`);
+        incrementComputerScore();
+
+    } else if (computerChoice === "Scissors" && playerChoice === "Scissors") {
+        alert(`You chose: ${playerChoice} \nThe computer chose: ${computerChoice} \nIt"s a draw!`);
+
+    } else {
+        alert(`Game error, please try again later.`);
+        throw `Game error! Please investigate`;
     }
 
 }
